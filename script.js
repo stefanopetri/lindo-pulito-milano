f47f71176fd07f0ab386ecf3ecb6b3db7ed3b58a
stefanopetri <stefano.nonez@gmail.com>
2026-08-03 21:44:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Lindo Pulito Milano");
+const canvas = document.getElementById("cleanCanvas");
+
+if (canvas) {
+
+    const ctx = canvas.getContext("2d");
+
+    const dirty = new Image();
+
+    dirty.src = "divano-sporco.PNG?v=2";
+
+    dirty.onload = () => {
+
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+
+        ctx.drawImage(dirty, 0, 0, canvas.width, canvas.height);
+
+    };
+
+}
